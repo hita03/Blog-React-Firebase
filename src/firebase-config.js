c336ccc863ddb466fe562e08fc771cb4ca6c2085
hita03 <hitajuneja@gmail.com>
2022-01-10 01:42:48 +0530

    Config files

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -8,13 +8,13 @@ import {getFirestore} from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAhUNKydkwPLEeXIQDv6NCW9b_HWm-tjEY",
-  authDomain: "blog-react-firebase-27bba.firebaseapp.com",
-  projectId: "blog-react-firebase-27bba",
-  storageBucket: "blog-react-firebase-27bba.appspot.com",
-  messagingSenderId: "371126650293",
-  appId: "1:371126650293:web:71806c1fb3b4d54434c5fc",
-  measurementId: "G-J9DGJM20PT"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 
